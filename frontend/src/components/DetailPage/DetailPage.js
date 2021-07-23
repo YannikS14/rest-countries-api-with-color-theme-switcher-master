c34b80724ac68a4693a07d858028e353dd264d11
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-07-23 18:40:19 +0200

    Add detail page component

--- frontend/src/components/DetailPage/DetailPage.js
+++ frontend/src/components/DetailPage/DetailPage.js
@@ -1,11 +1,52 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { StyledDetailPage } from './DetailPage.styled';
+import Button from '../Button/Button';
+import CountryDetails from '../CountryDetails/CountryDetails';
 
 function DetailPage({ match }) {
   const {
     params: { countryId },
   } = match;
 
-  return <div>Detail Page: {countryId}</div>;
+  const [country, setCountry] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getCountry = async () => {
+      setLoading(true);
+      setError(false);
+      try {
+        const response = await axios.get(
+          `https://restcountries.eu/rest/v2/alpha/${countryId}`,
+        );
+        setCountry(response.data);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+
+    getCountry();
+  }, [countryId]);
+
+  return (
+    <StyledDetailPage>
+      <Link to={`/`}>
+        <Button text="Back"></Button>
+      </Link>
+      {loading && (
+        <div>
+          Loading country details for country ID:{' '}
+          <strong>{countryId}</strong>
+        </div>
+      )}
+      {error && <div>Some error occured while fetching data</div>}
+      {country && <CountryDetails country={country} />}
+    </StyledDetailPage>
+  );
 }
 
 export default DetailPage;
--- frontend/src/components/DetailPage/DetailPage.styled.js
+++ frontend/src/components/DetailPage/DetailPage.styled.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
-export const StyledDetailPage = styled.main``;
+export const StyledDetailPage = styled.main`
+  padding: 3rem 0;
+  margin: 0 5rem;
+
+  @media (max-width: 600px) {
+    padding: 2rem 0;
+    margin: 0 2rem;
+  }
+`;
